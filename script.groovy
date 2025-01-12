9513d981c92a03fdf36ededdb501da27341febaa
Manh Trinh Nguyen <90488948+ManhTrinhNguyen@users.noreply.github.com>
2025-01-12 11:53:47 -0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,14 @@
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
+def buildApp(){
+    echo "Building application ...."
+    echo "Build app Version is ${params.VERSION}"
 }
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
-    }
+def testApp(){
+    echo "Testing application ...."
 }
 
-def deployApp() {
-    echo 'deploying the application...'
+def deployApp(){
+    echo "Deploying application ...."
 }
 
-return this
+return this 
